3e4c87e8b870f881867be889d5d3dcf8c06d10c1
ZeuS <ponzeadrian@gmail.com>
2024-03-05 19:13:10 +1100

    started on rps game

--- public/js/rpsgame.js
+++ public/js/rpsgame.js
@@ -1,1 +1,42 @@
-// 
+// Target DOM elements
+// still need to create these id's in html
+const choices = ['rock', 'paper', 'scissors'];
+const playerDisplay = document.getElementById('playerDisplay');
+const computerDisplay = document.getElementById('computerDisplay');
+const resultDisplay = document.getElementById('resultDisplay');
+
+const winCounter;
+const roundCounter;
+
+// Function for playing the game
+function playGame(playerChoice) {
+  const computerChoice = choices[Math.floor(Math.random() * 3)];
+  console.log(computerChoice);
+  let result = '';
+
+  // Validates if player choice is equal to computer choice
+  if (playerChoice === computerChoice) {
+    result = "It's a Tie!";
+  } else {
+    // Switch case for each possibility
+    switch (playerChoice) {
+      case 'rock':
+        result = computerChoice === 'scissors' ? 'You Win!' : 'You Lose';
+        break;
+      case 'paper':
+        result = computerChoice === 'rock' ? 'You Win!' : 'You Lose';
+        break;
+      case 'scissors':
+        result = computerChoice === 'paper' ? 'You Win!' : 'You Lose';
+        break;
+    }
+  }
+
+  // Displaying what the player and computer chose
+  playerDisplay.textContent = `PLAYER: ${playerChoice}`;
+  computerDisplay.textContent = `COMPUTER: ${computerChoice}`;
+  resultDisplay.textContent = result;
+}
+
+
+// Need to store who has won in the database for the leaderboard
