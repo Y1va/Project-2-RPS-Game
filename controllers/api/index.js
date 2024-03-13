5344c1e3ccd382ed9d98afdddf0965a743aeac6f
Jodie <jodielee062788@gmail.com>
2024-03-13 14:49:16 +1100

    update

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,13 +2,13 @@ const router = require('express').Router();
 
 const userRoutes = require('./UserRoutes')
 const communityForumRoutes = require('./comForumRoutes');
-// const leaderboardRoutes = require('./leaderboardRoutes');
-// const matchRoutes = require('./matchRoutes');
+const leaderboardRoutes = require('./leaderboardRoutes');
+const matchRoutes = require('./matchRoutes');
 
 // Define the routes
 router.use('/users', userRoutes)
 router.use('/communityforum', communityForumRoutes);
-// router.use('/leaderboard', leaderboardRoutes);
-// router.use('/match', matchRoutes);
+router.use('/leaderboard', leaderboardRoutes);
+router.use('/match', matchRoutes);
 
 module.exports = router;
--- controllers/api/matchRoutes.js
+++ controllers/api/matchRoutes.js
@@ -1,35 +1,35 @@
-// const router = require('express').Router();
-// const { Match } = require('../../models');
+const router = require('express').Router();
+const { Match } = require('../../models');
 
-// // Route to play match
-// router.post('/', async (req, res) => {
-//   try {
-//     const { userId, gamesPlayed, wins, losses } = req.body;
+// Route to play match
+router.post('/', async (req, res) => {
+  try {
+    const { userId, gamesPlayed, wins, losses } = req.body;
 
-//     // Record match result to the database
-//     const match = await Match.create({ userId, gamesPlayed, wins, losses });
+    // Record match result to the database
+    const match = await Match.create({ userId, gamesPlayed, wins, losses });
 
-//     // Send back the newly created match object to the client
-//     res.json(match);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: 'Internal server error' });
-//   }
-// });
+    // Send back the newly created match object to the client
+    res.json(match);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
-// // Route to get all matches for a specific user
-// router.get('/:userId', async (req, res) => {
-//   try {
-//     const { userId } = req.params;
+// Route to get all matches for a specific user
+router.get('/:userId', async (req, res) => {
+  try {
+    const { userId } = req.params;
 
-//     // Find all matches for the specified user
-//     const matches = await Match.findAll({ where: { userId } });
+    // Find all matches for the specified user
+    const matches = await Match.findAll({ where: { userId } });
 
-//     res.json(matches);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: 'Internal server error' });
-//   }
-// });
+    res.json(matches);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
-// module.exports = router;
+module.exports = router;
